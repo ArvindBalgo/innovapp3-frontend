f81c51066c332726f00ec26b591cf2dc83c7e588
prakash <prakash@ipedis.com>
2023-06-13 09:48:07 +0400

    add merchant component

--- src/app/pages/merchants/container/merchants.component.ts
+++ src/app/pages/merchants/container/merchants.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-merchants',
@@ -6,12 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MerchantsComponent implements OnInit {
 
-  constructor(
+  public merchants: any;
 
+  constructor(
+    private _httpClient: HttpClient,
+    private _router: Router
   ) { }
 
   ngOnInit(): void {
-
+    this._httpClient.get('/api/flights').subscribe((response: any) => {
+      this.merchants = response;
+      console.log('this.merchants', this.merchants);
+    })
   }
 
 }
