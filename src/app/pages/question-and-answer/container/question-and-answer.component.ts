0cfddab6b60ac9c46e970ec85d253c9d7f132704
Arvind <balgo_arvind@hotmail.com>
2023-06-18 09:00:16 +0400

    AI

--- src/app/pages/question-and-answer/container/question-and-answer.component.ts
+++ src/app/pages/question-and-answer/container/question-and-answer.component.ts
@@ -40,11 +40,25 @@ export class QuestionAndAnswerComponent implements OnInit {
 
   }
 
-  submitQuestion() {
+  submitQuestion(val: any) {
     // const textValue = event.target.value;
-    this._httpClient.post('https://mauriquotes-ai.herokuapp.com/ai/chat' , {query: 'kuma mo apeller'} ).subscribe((response: any) => {
+    this._httpClient.post('https://mauriquotes-ai.herokuapp.com/ai/chat' , {query: val} ).subscribe((response: any) => {
       console.log('response', response);
+      this.questionsList.push({
+        initiator: 'AI',
+        Message: response,
+        date: new Date()
+      })
     });
   }
 
+  submit($event: any) {
+    console.log($event.srcElement.value)
+    this.questionsList.push({
+      initiator: 'ME',
+      Message: $event.srcElement.value,
+      date: new Date()
+    })
+    this.submitQuestion($event.srcElement.value)
+  }
 }
